714ba1186b7aeaa5260cad2e27294f096ec7d5d9
yapivov2 <yapivov2@mts.ru>
2020-02-26 10:20:26 +0300

    added url

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/url"
 
 	"mai_scheduler/cmd"
 	"mai_scheduler/config"
@@ -18,6 +19,17 @@ func main() {
 	service.Config = cfg
 	service.InitClient()
 
-	service.GetGroupsByInstituteAndCourse("https://mai.ru/education/schedule/?department=151&course=3")
+	urlParse, err := url.Parse("https://mai.ru/education/schedule")
+	if err != nil{
+		fmt.Println("url!")
+		return
+	}
+	query := urlParse.Query()
+	query.Add("department","151")
+	query.Add("course", "3")
+
+	urlParse.RawQuery = query.Encode()
+
+	service.GetGroupsByInstituteAndCourse(urlParse.String())
 
 }
